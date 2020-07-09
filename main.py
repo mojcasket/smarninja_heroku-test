3975faf7fcbd4448cffae3b8ab85168c39c81d01
Mojca Sket <mateja.mojca.sket@gmail.com>
2020-07-09 18:44:17 +0200

    added files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
@@ -9,9 +9,23 @@ def index():
     cities = { "Vienna", "Ljublana", "Maribor"}
     return render_template ( "index.html", name=name, current_year=current_year, cities=cities )
 
-@app.route( "/about" )
+@app.route( "/about", methods=[ "GET" ])
 def about():
     return render_template( "about.html" )
+
+@app.route( "/contact", methods=[ "POST"])
+def contact():
+    contact_name = request.form.get( "contact-name" )
+    contact_surname = request.form.get( "contact-surname" )
+    contact_message = request.form.get ( " contact_message" )
+
+    return render_template( "success.html" )
+
+@app.route( "/message", methods=[ "POST"])
+def message():
+    contact_message = request.form.get ( "contact_message" )
+
+    return render_template( "message.html" )
 
 @app.route( "/base" )
 def base():
